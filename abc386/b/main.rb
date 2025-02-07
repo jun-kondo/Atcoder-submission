8fae9eb818e589a500d566c6d97876e95156f121
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-08 07:24:13 +0900

    ゼロと非ゼロのカウントロジックを実装
    入力文字列を処理し、ゼロの連続と非ゼロの数をカウントするロジックを追加。特にゼロで終わるケースにも対応し、結果を正確に計算できるよう修正。

--- abc386/b/main.rb
+++ abc386/b/main.rb
@@ -1,10 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+s = gets.to_s.chomp.chars
+res = 0
+z = 0
+s.each do |c|
+  if c == '0'
+    z += 1
+  else
+    # '00','0'をカウントできる
+    res += (z + 1) / 2
+    z = 0
+    # 0以外の数字をカウント
+    res += 1
+  end
+end
+# '32000'のようなゼロの連続で終わった場合、
+# 上のループではカウント出来ないので最後に加算する
+res += (z + 1) / 2
+puts res
