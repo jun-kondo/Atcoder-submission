9412c74c42bcb0956bfb71ebaef1d086162ec16d
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-09 16:45:31 +0900

    条件に基づくループ処理を追加
    入力値に基づいて条件を確認しカウントするロジックを実装しました。この変更により、指定条件を満たす入力データを正確に処理できるようになります。

--- abc376/a/main.rb
+++ abc376/a/main.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+n, c = gets.split.map(&:to_i)
+t = gets.split.map(&:to_i)
+res = 0
+diff = 0
+(0...n).each do |i|
+  if i.zero? || t[i] - diff >= c
+    res += 1
+    diff = t[i]
+  end
+end
+puts res
