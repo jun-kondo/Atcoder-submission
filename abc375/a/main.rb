6ce8cc55b39919268ac5942f97c3ae468bebc6a7
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-10 13:44:53 +0900

    文字列処理ロジックを追加して解析を実装
    入力文字列を解析し、特定のパターン("#.#")をカウントするロジックを追加。ループを用いた処理でパターンを検出し、結果を標準出力に出力します。

--- abc375/a/main.rb
+++ abc375/a/main.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
+n = gets.to_i
+s = gets.chomp.chars
+ans = 0
+now = 0
 
+while now < n
+  if s.slice(now, 3).join == '#.#'
+    ans += 1
+    now += 2
+  else
+    now += 1
+  end
+end
+puts ans
