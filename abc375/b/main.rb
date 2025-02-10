11e48d06ad3109ef72fb2b55791931c1fad95609
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-10 14:01:06 +0900

    距離計算ロジックを追加
    与えられた座標点を順番に巡回し、最後に始点に戻るまでの総距離を計算する処理を実装。lambdaを用いて距離計算を効率化。

--- abc375/b/main.rb
+++ abc375/b/main.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+cal_cost = ->(sx, sy, x, y) { Math.sqrt((sx - x)**2 + (sy - y)**2) }
+n = gets.to_i
+sx = 0
+sy = 0
+ans = 0
+n.times do
+  x, y = gets.split.map(&:to_i)
+  ans += cal_cost[sx, sy, x, y]
+  sx = x
+  sy = y
+end
+ans += cal_cost[sx, sy, 0, 0]
+puts ans
