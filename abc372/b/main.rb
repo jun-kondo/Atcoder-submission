62b638df3eddd2f0e5a72d63414b07f9805574ff
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-10 21:55:50 +0900

    三進数変換ロジックを追加
    入力を三進数に変換し、その桁ごとの値に基づいて次数を計算するロジックを実装。結果は回数配列として出力されるよう変更。

--- abc372/b/main.rb
+++ abc372/b/main.rb
@@ -3,7 +3,11 @@
 
 m = gets.to_i
 ans = []
+# 三進数に変換
 ternary = m.to_s(3)
+# iは3^NのNに相当。つまり次数
+# cは3進数なので0, 1, 2のどれか
+# 次数の数値を桁ごとの値の回数配列に追加する
 ternary.reverse.each_char.with_index { |c, i| ans.concat([i] * c.to_i) }
 puts ans.size
 puts ans.join(' ')
