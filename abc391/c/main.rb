56ef7e974b9c7ef4bff300ae95e4546a708d8882
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-11 14:00:54 +0900

    巣の移動処理を実装
    鳩の移動に伴い巣の羽数を管理する処理を追加し、2羽以上いる巣の数を効率的に更新するロジックを実装しました。これにより、クエリ処理のパフォーマンスが向上します。

--- abc391/c/main.rb
+++ abc391/c/main.rb
@@ -1,10 +1,34 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+n, q = gets.split.map(&:to_i)
+# 鳩No: 巣Noが初期値のハッシュ。各巣に1羽ずつが初期位置
+hole = (1..n).each_with_object({}) { |i, h| h[i] = i }
+# 巣No: 1が初期値のハッシュ
+cnt =  (1..n).each_with_object({}) { |i, h| h[i] = 1 }
+# 2羽以上いる巣の数
+ans = 0
+# countメソッドで2羽以上の巣の数を出力するとTLEになる
+# ので、巣の移動毎にansを更新する
+add_cnt = lambda { |h, x|
+  # - 変更前にcnt[h]が2以上だった場合、すでにそのキーの要素が重複してカウントされているので、更新前の状態をansから一度除外しておきます。
+  ans -= 1 if cnt[h] >= 2
+  cnt[h] += x
+  # - 更新後にcnt[h]が2以上であれば、重複状態が成立しているので、ansを1増やして新しい状態をカウントに反映させます
+  ans += 1 if cnt[h] >= 2
+  # 元々2以上であればプラマイゼロで何も変化しない。
+  # cnt[h]の更新前後のif文の結果が異なる場合、ansの値が増減する
+}
+q.times do
+  type, p, h = gets.split.map(&:to_i)
+  if type == 1
+    # 鳩piを取り出す。移動元の巣から-1羽する
+    add_cnt[hole[p], -1]
+    # 鳩ナンバー: 巣ナンバーのハッシュを更新する
+    hole[p] = h
+    # 鳩piをいれる。更新後の巣の羽数を+1する
+    add_cnt[hole[p], 1]
+  else
+    puts ans
+  end
+end
