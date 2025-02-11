1593f0ad4c96f031e609a801eab1332742bec9fd
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-11 22:27:22 +0900

    ヘビの移動ロジックを実装
    クエリ処理を追加して、ヘビの移動や座標計算を可能にしました。 "type"に応じて挙動を分岐させることで、追加、削除、位置計算の動作を実現しています。

--- abc389/c/main.rb
+++ abc389/c/main.rb
@@ -1,10 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+q = gets.to_i
+res = []
+last = 0
+index = 0
+q.times do
+  type, x = gets.split.map(&:to_i)
+  case type
+  when 1
+    # 配列resにはヘビの頭の座標が入る
+    res << last
+    last += x
+  when 2
+    # 先頭のヘビが抜ける
+    # 先頭のヘビの配列番号をインクリメントして更新する
+    index += 1
+  when 3
+    x -= 1
+    # indexの位置でオフセットを取る
+    puts res[index + x] - res[index]
+  end
+end
