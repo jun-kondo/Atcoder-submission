a19f85d2a6ff17fb387dd6ef2b2f8a0279467bff
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-11 20:00:18 +0900

    abc370 C問題の解答スクリプトを追加
    文字列の変換手順を求めるRubyスクリプトを新規作成。最小回数で変換するアルゴリズムを実装し、辞書順最小の処理も考慮。

---
+++ abc370/c/main.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+s = gets.chomp
+t = gets.chomp
+n = s.size
+ans = []
+while s != t
+  # eachの前に初期化
+  best = ''
+  # このeach文一周で1つの文字が書き換えられる
+  (0...n).each do |i|
+    next unless s[i] != t[i]
+
+    ns = s.dup
+    ns[i] = t[i]
+    # ループで書き換えた文字列をbestに格納
+    # bestは辞書順で一番小さい変更後の文字列が入る
+    best = if best == ''
+             ns
+           else
+             # 辞書順で前の方を取り出す
+             [best, ns].min
+           end
+  end
+  # bestでsを上書きする。
+  s = best
+  ans << s
+end
+puts ans.size
+puts ans
