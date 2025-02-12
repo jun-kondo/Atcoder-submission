b809163646006484f7e641190b08d7320c23a6fd
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-12 14:34:11 +0900

    配列処理ロジックを実装
    配列の2倍条件を満たす要素のペアを数えるロジックを追加。ループと条件分岐を使用して効率的に計算を行う形に改善。

--- abc388/c/main.rb
+++ abc388/c/main.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
+n = gets.to_i
+a = gets.split.map(&:to_i)
 
+ans = 0
+r = 1
+n.times do |l|
+  r += 1 while r < n && a[r] / a[l] < 2
+  ans += n - r
+end
+puts ans
