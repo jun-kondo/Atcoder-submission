d2361aa0d5e56899070aa255c69aa3c303211506
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-12 21:57:42 +0900

    ビット全探索の実装を追加
    配列に基づいてビット全探索で組み合わせを計算し、結果をソートして出力するロジックを追加。可読性向上のためコメントを充実させ、コードを整理しました。

--- abc374/c/main.rb
+++ abc374/c/main.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-n = gets.to_i
-k = gets.split.map(&:to_i)
+n = gets.to_i # 5
+k = gets.split.map(&:to_i) # 2 3 5 10 12
 
 ans = 10**9
+# ビットを左シフト。n=5なら32になる(1...32)なので32-1
 (1...(1 << n)).each do |s|
   a = 0
   b = 0
-  n.times do |i|
+  (0...n).each do |i|
     if (s >> i) & 1 == 1
       b += k[i]
     else
--- abc384/c/main.rb
+++ abc384/c/main.rb
@@ -1,10 +1,28 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i) # 入力値: 400 500 600 700 800
+n = a.size # 入力値は5個
+ranking = []
 
+# bit全探索
+# 今回0はありえないので1~31を探索する
+(1..31).each do |s|
+  name = ''
+  score = 0
+  # ビット全探索内側ループ
+  # 0 ~ n-1の各ビット位置をループ
+  (0...n).each do |i| # 今回はi: 0~4。5桁のビットだから
+    # ビットシフトとビットAND演算により、s の i 番目のビットが 1 かどうかを判定する
+    next unless (s >> i) & 1 == 1
+
+    # i 番目のビットが立っている場合、配列 a の i 番目の値を score に加算する
+    score += a[i]
+    # 対応するアルファベット（'A' から順に）を name に追加する
+    name += ('A'.ord + i).chr
+  end
+  ranking << [-score, name] # ['A', 400]みたいな名前、スコアのペア配列
+  # 問題の仕様で「数値は降順、数値が同じ場合は文字列は昇順」なので
+  # scoreの数値を反転させることでsortメソッドで降順にソートできる
+end
+ranking.sort.each { |r| puts r[1] }
