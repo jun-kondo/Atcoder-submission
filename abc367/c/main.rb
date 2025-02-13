a5ac774402f0c1e747dd0f5c9aaafeae5f893a67
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-13 11:26:13 +0900

    ABC367 Cの解法を実装
    入力をもとに全組み合わせを生成し、総和が指定の倍数となる組み合わせを列挙するロジックを追加。結果を辞書順にソートして出力するよう変更しました。

--- abc367/c/main.rb
+++ abc367/c/main.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+# https://atcoder.jp/contests/abc367/tasks/abc367_c
+_n, k = gets.split.map(&:to_i) # 3 2
+r = gets.split.map(&:to_i) # 2 1 3
+r = r.map { |n| (1..n).to_a } # [[1, 2], [1], [1,2,3]]にする
+# ↑で作ったrの各要素すべての組み合わせを作成する => product
+# [[1,1,1],[2,1,2]...[2,1,3]]みたいな配列が出来る
+# selectで数列の総和がkの倍数になるものを列挙する
+ans = r[0].product(*(r[1..])).select { |n| (n.sum % k).zero? }
+# 一つの二重配列から全要素をproductで全列挙するときは
+# 最初の要素をレシーバーにして引数に残りの要素を入れる => *(r[1..])
+ans.sort.each { |n| puts n.join(' ') } # 辞書順に並べて出力
