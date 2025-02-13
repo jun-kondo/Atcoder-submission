6fcebad8033e098db076db7cace2284a7d28099a
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-13 14:06:36 +0900

    二分探索の処理を Range#bsearch に置き換え
    二分探索ロジックを簡潔化するため Range#bsearch を使用。従来のwhileループを置き換えることでコードの可読性と保守性を向上させた。また、不要なコメントを削除し、処理の意図を明確にした。

--- abc365/c/main.rb
+++ abc365/c/main.rb
@@ -4,24 +4,15 @@
 n, m = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 
-f = lambda { |x|
-  s = a.sum { |ai| [x, ai].min }
-  s <= m
-}
+# 条件チェック用のラムダ
+f = ->(x) { a.sum { |ai| [x, ai].min } <= m }
 INF = 1_001_001_001
-if f[INF] # 入力値の配列aの合計がmを超えてないか確認 => 超えているとLambda内のxに何を代入しても成立してしまう
+
+if f[INF]
+  # 無限大に対して条件が成立する場合は "infinite" を出力
   puts 'infinite'
 else
-  # 二分探索
-  ac = 0 # 下限
-  wa = INF # 上限
-  while ac + 1 < wa # 上限と下限が隣り合わせになるまで
-    wj = (ac + wa) / 2 # waiting judge: 判定ポイント 問題文でのxに相当
-    if f[wj]
-      ac = wj # 下限をwjに寄せる
-    else
-      wa = wj # 上限をwjに寄せる
-    end
-  end
-  puts ac
+  # Range#bsearch を利用して、条件を満たさなくなる最小の x を求め、その前の値が求める値
+  smallest_false = (0..INF).bsearch { |x| !f[x] }
+  puts smallest_false - 1
 end
