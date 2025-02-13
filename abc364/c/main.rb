5950a7f9d55e899753a434dde06a3bafba575280
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-13 20:25:36 +0900

    制約チェックと早期終了ロジックを実装
    入力値に基づいて合計値が閾値を超えた時点でループを終了する仕組みを追加。パフォーマンス向上のため、直接終了処理を呼び出す設計に変更。

--- abc364/c/main.rb
+++ abc364/c/main.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
-
+n, x, y = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i).sort.reverse
+b = gets.split.map(&:to_i).sort.reverse
+tot_a = 0
+tot_b = 0
+(0...n).each do |i|
+  tot_a += a[i]
+  tot_b += b[i]
+  if tot_a > x || tot_b > y
+    puts i + 1
+    exit
+  end
+end
+puts n
