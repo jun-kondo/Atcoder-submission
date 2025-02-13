c7f779dc7acfb3bd32759fbad43ed3b49e374dbe
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-13 13:47:19 +0900

    二分探索を実装してアルゴリズムを最適化
    入力処理を修正し、二分探索による解法を追加。条件判定をLambdaで定義し、入力制約を考慮したロジックを実現。特定条件下の無限大ケースもハンドリング。

--- abc365/c/main.rb
+++ abc365/c/main.rb
@@ -1,10 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
+n, m = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
 
+f = lambda { |x|
+  s = a.sum { |ai| [x, ai].min }
+  s <= m
+}
+INF = 1_001_001_001
+if f[INF] # 入力値の配列aの合計がmを超えてないか確認 => 超えているとLambda内のxに何を代入しても成立してしまう
+  puts 'infinite'
+else
+  # 二分探索
+  ac = 0 # 下限
+  wa = INF # 上限
+  while ac + 1 < wa # 上限と下限が隣り合わせになるまで
+    wj = (ac + wa) / 2 # waiting judge: 判定ポイント 問題文でのxに相当
+    if f[wj]
+      ac = wj # 下限をwjに寄せる
+    else
+      wa = wj # 上限をwjに寄せる
+    end
+  end
+  puts ac
+end
