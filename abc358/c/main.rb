d275e84aa19c1e49bb738d8a42d62528a7ec0469
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-14 18:41:03 +0900

    ABC358-C: 全探索で最小店舗数を求める処理を実装
    各店舗の組み合わせを全探索してすべての味をカバーする最小の店舗数を算出するロジックを実装。ビット演算を活用して訪問店舗数を効率よく計算しています。

--- abc358/c/main.rb
+++ abc358/c/main.rb
@@ -1,8 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+# https://atcoder.jp/contests/abc358/tasks/abc358_c
+n, m = gets.split.map(&:to_i)
+s = Array.new(n) { gets.chomp }
+ans = n
+# 全ての売り場に対して行ったor行かなかったの組み合わせ(部分集合)を全探索する
+(0...1 << n).each do |x|
+  # 売り場の組み合わせ（部分集合）で**すべての味がカバーできるかどうか**の状態
+  ok = true
+  # フレーバー毎に各売り場をチェックする
+  (0...m).each do |i|
+    # **特定の味について**、
+    # 現在選択している売り場の中にその味を扱っているものがあるかどうか
+    can = false
+    (0...n).each do |j|
+      # その売場を訪れた、かつ売り場にフレーバーがあったかどうか判定
+      can = true if x >> j & 1 == 1 && (s[j][i] == 'o')
+    end
+    ok = false unless can
+  end
+  # 立っているビットの本数を数えている
+  # この場合ビット=店を訪れたということ。より少ないビット本数の結果を求める
+  ans = [ans, x.to_s(2).count('1')].min if ok
+end
+puts ans
