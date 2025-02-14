6ae9d655e0a176a9ad719c2feab66c1a9877cbfe
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-14 15:38:44 +0900

    重複する重量の最大値を除外して合計を計算
    入力を解析し、重複する重量の最大値を除外して合計を算出するロジックを追加しました。これにより条件に合致するデータ処理が可能になりました。

--- abc360/c/main.rb
+++ abc360/c/main.rb
@@ -1,8 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
+n = gets.to_i
 a = gets.split.map(&:to_i)
+w = gets.split.map(&:to_i)
+ws = {}
+(0...n).each do |i|
+  ai = a[i]
+  wi = w[i]
+  if ws[ai]
+    ws[ai] << wi
+  else
+    ws[ai] = [wi]
+  end
+end
+
+ans = 0
+ws.each do |_ai, wi|
+  next if wi.size < 2
+
+  ans += wi.sum - wi.max
+end
+puts ans
