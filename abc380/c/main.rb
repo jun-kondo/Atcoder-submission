d0299fa3a27bdc949d5ef1247d30426e3822e3a3
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-14 12:41:51 +0900

    ランレングス圧縮を用いたロジックに変更
    文字列をランレングス圧縮で処理し、k番目の'1'ブロックを位置交換で操作可能に変更。これによりコードの簡潔性と可読性を向上。

--- abc380/c/main.rb
+++ abc380/c/main.rb
@@ -2,20 +2,25 @@
 # frozen_string_literal: true
 
 n, k = gets.split.map(&:to_i)
-s = gets.chomp.chars
-res = []
-from = nil
-(0...n).each do |i|
-  if s[i].to_i == 1 && from.nil?
-    from = i
-    res << { from:, to: i, dist: 0 }
-  elsif s[i].to_i == 1
-    res.last[:to] = i
-    res.last[:dist] += 1
+s = gets.chomp # 010011100011001
+# 0 *n, 1*nの塊に分けて考える。=>  0*1, 1*1, 0*2, 1*3, 0*3, 1*2, 0*2, 1*1
+# run length encoding
+rle = []
+s.each_char do |c|
+  if rle.size > 0 && rle[-1][:char] == c
+    rle[-1][:length] += 1
   else
-    from = nil
+    rle << { char: c, length: 1 } # 文字、長さのペアを作る
   end
 end
-(res[k - 1][:from]..res[k - 1][:to]).each { |i| s[i] = '0' }
-(res[k - 2][:to] + 1..res[k - 2][:to] + 1 + res[k - 1][:dist]).each { |i| s[i] = '1' }
-puts s.join
+
+one = 0
+rle.each_with_index do |e, i|
+  next unless e[:char] == '1'
+
+  one += 1 # charが'1'の要素をカウント
+  rle[i - 1], rle[i] = rle[i], rle[i - 1] if one == k # すわっぷ
+end
+# 復元
+ans = rle.map { |e| e[:char] * e[:length] }.join
+puts ans
