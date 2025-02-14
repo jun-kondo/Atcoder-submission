2572b9be100b2ed025ad2651ed19a58567a6e0dc
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-14 13:59:39 +0900

    数列処理ロジックを実装
    入力された数列をソートし、全探索でkに基づく最小値を計算するロジックを追加。計算結果を出力して、要件に基づいた解決を図った。

--- abc361/c/main.rb
+++ abc361/c/main.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
+n, k = gets.split.map(&:to_i) # 5 2
+a = gets.split.map(&:to_i).sort # 3 1 5 4 9
 
+# ソートした数列のサイズ-kの両端の差を算出
+# 最小値を比較する。を(0..k)の範囲で行う
+ans = 10**9 + 1
+(0..k).each do |i|
+  d = a[n - 1 - k + i] - a[i]
+  ans = [ans, d].min
+end
+puts ans
