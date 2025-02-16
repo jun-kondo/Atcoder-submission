b13367212e6c36456ac12e86165832e4de61b29c
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-16 23:43:44 +0900

    abc393 c
    入力処理を拡張し、重複チェックとカウントロジックを実装しました。`Set`を活用することで効率的に重複を管理し、出力結果が正確になるよう改善しました。

--- abc393/c/main.rb
+++ abc393/c/main.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+n, m = gets.split.map(&:to_i)
+exist = Set.new
+ans = 0
+
+m.times do
+  a, b = gets.split.map(&:to_i)
+  if a == b
+    ans += 1
+    next
+  end
+  ans += 1 unless exist.add?([a, b].sort)
+end
+puts ans
