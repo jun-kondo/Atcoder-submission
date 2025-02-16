55bc6480d5d0041d207ac6799306ba54f6524350
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-16 23:45:15 +0900

    abc362 b
    三平方の定理を利用し、与えられた三点が直角三角形を形成するかどうかを判定する処理を実装しました。この変更により、指定された問題の要件を満たすことができます。

--- abc362/b/main.rb
+++ abc362/b/main.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+# https://atcoder.jp/contests/abc362/tasks/abc362_b
+ax, ay = gets.split.map(&:to_i)
+bx, by = gets.split.map(&:to_i)
+cx, cy = gets.split.map(&:to_i)
+
+# 三平方の定理
+ab2 = (ax - bx)**2 + (ay - by)**2
+bc2 = (bx - cx)**2 + (by - cy)**2
+ca2 = (ax - cx)**2 + (ay - cy)**2
+# 直角を挟んだ二点の距離の二乗の合計と斜辺の二乗の距離が等しい
+if ab2 + bc2 == ca2 || ab2 + ca2 == bc2 || bc2 + ca2 == ab2
+  puts 'Yes'
+else
+  puts 'No'
+end
