74cdafaa6d476fbb01f02b5bbb2884d21188a0eb
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-16 23:43:14 +0900

    abc393 d
    文字列から特定の条件を満たす位置を抽出し、それらの位置に基づいて計算を行うロジックを追加。計算は中心位置を基準にして距離を求める形に変更。不要なコメント行を削除し、コードを簡潔化。

--- abc393/d/main.rb
+++ abc393/d/main.rb
@@ -1,8 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+n = gets.to_i
+s = gets.chomp.chars
+h = []
+s.each_with_index do |c, i|
+  h << i if c == '1'
+end
+ans = 0
+center = h.size / 2
+ofs = 1
+h.each do |p|
+  if p == h[center]
+    ofs = 1
+    next
+  end
+
+  ans += (p - h[center]).abs - ofs
+  ofs += 1
+end
+puts ans
