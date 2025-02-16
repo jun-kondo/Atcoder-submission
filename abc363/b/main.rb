23f9d727273dc67dbcd293b6e586e7b81b490ca1
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-16 23:45:37 +0900

    abc363 b
    特定条件を満たすカウントを計算し、不足分を補うために値を更新する処理を追加しました。`gets`で入力を取得し、ループ内でカウントおよびリスト更新を行う形式に修正しています。これにより正しい結果を出力可能としました。

--- abc363/b/main.rb
+++ abc363/b/main.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+n, t, p = gets.split.map(&:to_i)
+l = gets.split.map(&:to_i)
+ans = 0
+cnt = l.count { |x| x >= t }
+while cnt < p
+  (0...n).each do |i|
+    cnt -= 1 if l[i] >= t
+    l[i] += 1
+    cnt += 1 if l[i] >= t
+  end
+  ans += 1
+end
+puts ans
