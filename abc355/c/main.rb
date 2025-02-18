2bc09d73073ed8d9a6c48f1f54a9df3623df95cc
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-18 15:02:08 +0900

    abc355 c
    入力を基にビンゴの判定を行うロジックを追加。行、列、斜めのカウンタを用いて条件を満たした際に結果を出力する仕様に変更。

--- abc355/c/main.rb
+++ abc355/c/main.rb
@@ -1,8 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+# https://atcoder.jp/contests/abc355/tasks/abc355_c
+# 1..nの数値が順番に並んだビンゴ
+n, t = gets.split.map(&:to_i)
+# 行と列分のカウンタを作る
+# カウンタの数値がnと等しくなればBingo判定
+rows = Array.new(n, 0)
+cols = Array.new(n, 0)
+# 斜めカウンタ,これもnと等しくなればビンゴ
+dia1 = 0
+dia2 = 0
+as = gets.split.map(&:to_i)
+t.times do |k|
+  a = as.shift
+  a -= 1
+  i = a / n # 行を求める
+  j = a % n # 列を求める
+  rows[i] += 1
+  cols[j] += 1
+  dia1 += 1 if i == j
+  dia2 += 1 if i + j == n - 1
+  if rows[i] == n || cols[j] == n || dia1 == n || dia2 == n
+    puts k + 1
+    exit
+  end
+end
+puts '-1'
