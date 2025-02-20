feb8520d5e0c48a4811c35b595adda513ac4560b
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-21 00:05:43 +0900

    abc345 abc
    各問題に対して適切な入力処理と解法を実装。問題Aでは正規表現を使用した判定、問題Bでは切り上げ計算、問題Cでは組み合わせ計算を通じた解答を実現。コメントも追加し可読性を向上。

--- abc345/c/main.rb
+++ abc345/c/main.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+# https://atcoder.jp/contests/abc345/tasks/abc345_c
+# 文字列から任意の2文字を選択して入れ替えたあとの文字列の数を答える
+s = gets.chomp.chars
+n = s.size
+# 文字列sの各文字をカウント
+cnt = s.tally
+# 同じ文字を2つ選択したときの組み合わせをカウントしている。
+same = cnt.values.sum { |v| v * (v - 1) / 2 }
+# 組み合わせの総数 ‐ 入れ替えても変化しない(同じ文字を入れ替える)組み合わせ
+diff = n * (n - 1) / 2 - same
+ans = diff
+ans += 1 unless same == 0
+puts ans
