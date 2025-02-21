03ca4707956a1fe45c1a9a8d8320bb7427d59784
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-21 21:55:10 +0900

    abc361 a,b
    配列操作として要素の挿入処理を実装し、範囲判定に3次元座標を考慮したロジックを追加しました。これにより、データ操作の精度と判定機能が向上しています。

--- abc361/b/main.rb
+++ abc361/b/main.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# n = gets.to_s.to_i  # 整数1つを受け取る(1行に1つ整数がある前提)#
-# s = gets.to_s.chomp # 1行を文字列として受け取る(chompで最後の改行を切り落としている)
-# a = gets.to_s.split.map{ |e| e.to_i } # 横1行のスペース区切りの整数を配列として受け取る
-# a = Array.new(n){ gets.to_s.to_i } # n行1列の改行区切りの整数を配列として受け取る
-# m = Array.new(n){ gets.to_s.split.map{ |e| e.to_i } } # n行m列の整数を2次元配列で受け取る
-a, b = gets.split.map(&:to_i)
+Point = Struct.new(:x, :y, :z)
+c1 = gets.split.map(&:to_i)
+c2 = gets.split.map(&:to_i)
 
+l1 = Point.new(c1[0], c1[1], c1[2])
+r1 = Point.new(c1[3], c1[4], c1[5])
+l2 = Point.new(c2[0], c2[1], c2[2])
+r2 = Point.new(c2[3], c2[4], c2[5])
+if r1.x <= l2.x || r2.x <= l1.x || r1.y <= l2.y || r2.y <= l1.y || r1.z <= l2.z || r2.z <= l1.z
+  puts('No')
+else
+  puts('Yes')
+end
