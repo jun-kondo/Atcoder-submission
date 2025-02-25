57c9990af3795b8bb03a7bda496254733c453857
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-26 03:58:18 +0900

    abc362 a,c
    「C」では配列の構造化と加算条件の判定を実装し、出力処理を変更しました。「A」では色に応じたケース分けのロジックを導入し、最小値を出力する処理を追加しました。

--- abc362/a/main.rb
+++ abc362/a/main.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+r, g, b = gets.split.map(&:to_i)
+c = gets.chomp
+
+pens = case c
+       when 'Red' then [g, b]
+       when 'Green' then [r, b]
+       when 'Blue' then [r, g]
+       else
+         [r, g, b]
+       end
+puts pens.min
--- abc362/c/main.rb
+++ abc362/c/main.rb
@@ -1,8 +1,35 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+n = gets.to_i
+# [l, r]の配列をn個ではなくn個の配列LとRを作る
+left = []
+right = []
+(0...n).each do
+  li, ri = gets.split.map(&:to_i)
+  left << li
+  right << ri
+end
+
+sum_left = left.sum
+sum_right = right.sum
+# Lの合計 < 0 < Rの合計になっていれば条件を満たしている
+if sum_left > 0 || sum_right < 0
+  puts 'No'
+  exit
+end
+puts 'Yes'
+
+ans = left
+remain = -sum_left
+(0...n).each do |i|
+  can_add = right[i] - left[i]
+  if can_add < remain
+    ans[i] = right[i] # ansはlの値なのでそれをrに置き換える. l[i] + can_addでr[i]
+    remain -= can_add
+  else
+    ans[i] += remain
+    break
+  end
+end
+puts ans.join(' ')
