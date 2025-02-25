f083a606901d5a3356016c8764bb91f6b4af8adb
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-25 22:19:32 +0900

    abc337 abc

--- abc337/a/main.rb
+++ abc337/a/main.rb
@@ -1,8 +1,20 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+n = gets.to_i
+a = 0
+b = 0
+
+(0...n).each do
+  x, y = gets.split.map(&:to_i)
+  a += x
+  b += y
+end
+
+if a > b
+  puts 'Takahashi'
+elsif b > a
+  puts 'Aoki'
+else
+  puts 'Draw'
+end
--- abc337/c/main.rb
+++ abc337/c/main.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
+n = gets.to_i
 a = gets.split.map(&:to_i)
+hash = {}
+(0...n).each { |i| hash[a[i]] = i + 1 }
+
+ans = []
+ans << hash[-1]
+(n - 1).times { ans << hash[ans[-1]] }
+
+puts ans.join(' ')
