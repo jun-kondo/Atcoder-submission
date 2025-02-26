e483b06099ffa0619d4a17e2605d48b056058409
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-27 06:07:57 +0900

    ABC332 bc
    aは初回セットアップ時に一緒にコミットしちゃた

--- abc332/b/main.rb
+++ abc332/b/main.rb
@@ -1,8 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+k, g, m = gets.split.map(&:to_i)
+
+glass = 0
+mug = 0
+(0...k).each do
+  if glass == g
+    glass = 0
+  elsif mug == 0
+    mug = m
+  else
+    p = [g - glass, mug].min # ここの実装がわからなかった
+    glass += p
+    mug -= p
+  end
+end
+
+puts "#{glass} #{mug}"
+
+# グラスとマグカップ間で水を移し替える問題
--- abc332/c/main.rb
+++ abc332/c/main.rb
@@ -1,8 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+n, m = gets.split.map(&:to_i)
+s = gets.chomp.chars
+
+stock = m
+ans = 0
+new = 0
+(0...n).each do |i|
+  if s[i] == '2'
+    new += 1
+  elsif s[i] == '1' && stock > 0
+    stock -= 1
+  elsif s[i] == '1'
+    new += 1
+  else
+    stock = m
+    new = 0
+  end
+  ans = [ans, new].max
+end
+puts ans
+
+# 洗濯するまでに着回すために購入するTシャツの枚数を求める問題
+# nの数が少なかったのでシミュレーション
