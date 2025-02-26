3422c8027b5ffc82dedab3532883eb0e5e2aa769
jun-kondo <ranmaru.oo.q@gmail.com>
2025-02-26 21:43:05 +0900

    ABC333 abc
    B - Pentagon
    C - Repunit Trio

--- abc333/b/main.rb
+++ abc333/b/main.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+a, b = gets.chomp.chars.map(&:ord)
+c, d = gets.chomp.chars.map(&:ord)
+
+near = lambda { |x, y|
+  x, y = y, x if x > y
+  y - x == 1 || y - x == 4
+}
+if near[a, b] == near[c, d]
+  puts 'Yes'
+else
+  puts 'No'
+end
--- abc333/c/main.rb
+++ abc333/c/main.rb
@@ -1,8 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# a = Array.new(n){ gets.to_i }
-# m = Array.new(n){ gets.split.map(&:to_i) }
-# n = gets.to_i
-# s = gets.chomp
-a = gets.split.map(&:to_i)
+# https://atcoder.jp/contests/abc333/tasks/abc333_c
+# レピュニット数
+n = gets.to_i
+
+l = 12 # サンプルからnの最大値の結果は12桁だった
+
+repunit = (0...l).map { |i| ('1' * (i + 1)).to_i } # 1, 11, 111, 1111...
+s = Set.new
+(0...l).each do |i|
+  (0...l).each do |j|
+    (0...l).each do |k|
+      s.add(repunit[i] + repunit[j] + repunit[k])
+    end
+  end
+end
+puts s.to_a.sort[n - 1]
